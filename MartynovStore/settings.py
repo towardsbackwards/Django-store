f13be56dffa0527f80460562555b3fa12a637db5
pashoid <azotas@list.ru>
2019-09-09 15:25:29 +0300

    Styles updated for adminapp & menu dropdowns

--- MartynovStore/settings.py
+++ MartynovStore/settings.py
@@ -89,6 +89,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    #'default': {
+     #   'NAME': 'geekshop',
+      #  'ENGINE': 'django.db.backends.postgresql',
+       # 'USER' : 'django',
+        #'PASSWORD' : 'geekbrains'
+    #}
 }
 
 
