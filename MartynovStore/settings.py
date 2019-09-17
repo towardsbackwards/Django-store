84e3f97cfb7dc7d5b34478f1083433d064d332f5
pashoid <azotas@list.ru>
2019-09-17 18:37:58 +0300

    Low cache settings added

--- MartynovStore/settings.py
+++ MartynovStore/settings.py
@@ -230,3 +230,17 @@ if DEBUG:
 
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+if os.name == 'posix':
+    CACHE_MIDDLEWARE_ALIAS = 'default'
+    CACHE_MIDDLEWARE_SECONDS = 120
+    CACHE_MIDDLEWARE_KEY_PREFIX = 'geekbrains'
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+        }
+    }
+
+LOW_CACHE = True
